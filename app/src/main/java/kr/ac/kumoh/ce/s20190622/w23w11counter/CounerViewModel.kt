6720b195c37b01a3b49dcd7d8cfbe385e2e76519
Mne <42082748+Mne-pr@users.noreply.github.com>
2023-11-16 14:26:20 +0900

    final

--- app/src/main/java/kr/ac/kumoh/ce/s20190622/w23w11counter/CounerViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190622/w23w11counter/CounerViewModel.kt
@@ -4,12 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CounerViewModel : ViewModel() {
+class CounterViewModel : ViewModel() {
     private val _count = MutableLiveData(0)
     val count: LiveData<Int> = _count
 
     fun onAdd(){
-        if((_count.value ?: 0) > 0) // !!와 비슷한처리
+        if((_count.value ?: 0) >= 0) // !!와 비슷한처리
             _count.value = _count.value!! + 1
+    }
+
+    fun onSub(){
+        if((_count.value ?: 0) >= 0) // !!와 비슷한처리
+            _count.value = _count.value!! - 1
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20190622/w23w11counter/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190622/w23w11counter/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,29 +18,33 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190622.w23w11counter.ui.theme.W23W11CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val vm = ViewModelProvider(this)[CounterViewModel::class.java]
+        val vm2 = ViewModelProvider(this)[CounterViewModel::class.java] // viewModel에서 가져오는 값은 동일하므로 의미없는짓
+
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
                 Column (
-                    Modifier.fillMaxSize().padding(8.dp),
+                    Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
                     verticalArrangement = Arrangement.Center,
                 ) {
-                    Clicker()
-                    Counter()
+                    Counter(vm)
+                    Counter(vm2) // viewModel은 싱글톤 객체이기 때문에.. 계속 똑같은 값을 가져옴
                 }
             }
         }
@@ -86,9 +89,10 @@ fun Clicker(){
 }
 
 @Composable
-fun Counter(){
+fun Counter(viewModel: CounterViewModel){
     // 값이 바뀔 때 재구성-recomposition이 일어나야 함 => 변수를 상태로 만들어야 함
-    var (c, setC) = rememberSaveable { mutableStateOf(value=0) }
+    //var (c, setC) = rememberSaveable { mutableStateOf(value=0) }
+    val count by viewModel.count.observeAsState(0)
 
     Column(modifier = Modifier
         .fillMaxWidth()
@@ -96,20 +100,22 @@ fun Counter(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "$c", fontSize = 70.sp,)
+        Text(text = "$count", fontSize = 70.sp,)
 
         Row(modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             ) {
             Button(modifier = Modifier
                 .height(50.dp)
-                .weight(1f), onClick = { setC( c+1 )}) {
+                .weight(1f),
+                onClick = { viewModel.onAdd() }) {
                 Text("증가")
             }
             Spacer(modifier = Modifier.width(10.dp))
             Button(modifier = Modifier
                 .height(50.dp)
-                .weight(1f), onClick = { setC( c-1 ) }) {
+                .weight(1f),
+                onClick = { viewModel.onSub() }) {
                 Text("감소")
             }
         }
